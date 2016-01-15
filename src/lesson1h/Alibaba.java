f1a2571c2a69e68364796c98ecd34c70a458122d
Solomin Andrey <solomin.andrey@gmail.com>
2016-01-15 17:39:27 +0300

    Простая программа по мотивам анекдота про алибабу и сорок разбойников

---
+++ src/lesson1h/Alibaba.java
@@ -0,0 +1,32 @@
+package lesson1h;
+
+public class Alibaba {
+    public static void main(String[] args) {
+        greeting("разбойник", 40);
+        saybb("разбойник", 40);
+    }
+
+    private static void saybb(String person, int col) {
+        if (col == 1) {
+            System.out.printf("До свидания %s!", person);
+            System.out.println();
+        } else {
+            for (int i = 1; i <= col; i++) {
+                System.out.printf("До свидания %s номер %d\n", person, i);
+                saybb("колобок", 1);
+            }
+        }
+    }
+
+    private static void greeting(String person, int col) {
+        if (col == 1) {
+            System.out.printf("Здравствуй %s!", person);
+            System.out.println();
+        } else {
+            for (int i = 1; i <= col; i++) {
+                System.out.printf("Здравствуй %s номер %d\n", person, i);
+                greeting("колобок", 1);
+            }
+        }
+    }
+}
